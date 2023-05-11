976bcc292bba18d266ded44816644de45c7b846f
rogercerpa <roger.e.cerpa@gmail.com>
2023-05-10 23:07:21 -0400

    added a landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Sidebar from '../Components/Sidebar';
 import ProjectCreation from '../Features/ProjectCreation';
 import QueueStatus from '../Features/QueueStatus';
+import LandingPage from '../Pages/LandingPage';
 import { Route, Routes } from 'react-router-dom';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Sidebar />
       <div className="flex flex-col w-full">
         <Routes>
+          <Route exact path="/" component={LandingPage} />
           <Route path="/ProjectCreation" component={ProjectCreation} />
           <Route path="/QueueStatus" component={QueueStatus} />
           {/* Add more routes for additional features */}
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold text-black ">Welcome to the Application</h1>
+      <p>Please select a feature from the sidebar to get started.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
