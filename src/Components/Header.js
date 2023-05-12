e36e21a891c810a078d94852333dcc92cc6a60ea
rogercerpa <roger.e.cerpa@gmail.com>
2023-05-11 22:54:46 -0400

    updated styling for the header logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from "../assets/logo.png"
 // const electron = window.require('electron');
 // const ipcRenderer  = electron.ipcRenderer;
 
@@ -22,10 +24,13 @@ const Header = () => {
 
   return (
     <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="font-bold text-xl">DAS Tools App</h1>
-        <button  className="bg-blue-700 hover:bg-blue-800 text-white py-2 px-4 rounded">
-        Import Data
+      <div className="container mx-auto flex justify-between items-center flex-col sm:flex-row">
+        <Link to="/" className="flex flex-col items-center mb-2 sm:mb-0">
+          <img src={Logo} alt="Logo" className="w-10 h-10 object-cover" />
+          <h1 className="text-xs mt-2">All Tools in One</h1>
+        </Link>
+        <button className="bg-[#5A0] hover:bg-[#4d7427] text-white py-2 px-4 rounded">
+          Import Data
         </button>
       </div>
     </header>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,21 +6,21 @@ const Sidebar = () => {
     <div className="bg-gray-200 h-screen w-64 p-4">
       <NavLink
         to="/"
-        className="block mb-4"
+        className="block mb-4 hover:bg-[#5A0] py-2 px-4 rounded"
         activeClassName="text-blue-500"
       >
         Home
       </NavLink>
       <NavLink
         to="/ProjectCreation"
-        className="block mb-4"
+        className="block mb-4 hover:bg-[#5A0] py-2 px-4 rounded"
         activeClassName="text-blue-500"
       >
         Project Creation
       </NavLink>
       <NavLink
         to="/QueueStatus"
-        className="block mb-4"
+        className="block mb-4 hover:bg-[#5A0] py-2 px-4 rounded"
         activeClassName="text-blue-500"
       >
         Queue Status
