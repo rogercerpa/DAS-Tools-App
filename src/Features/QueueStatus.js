75f024d1cb86423b7e1753ed8cf55a8d1fce5fdd
rogercerpa <roger.e.cerpa@gmail.com>
2023-05-16 22:14:37 -0400

    set up the project to use electron store

--- src/Features/QueueStatus.js
+++ src/Features/QueueStatus.js
@@ -1,12 +1,31 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const QueueStatus = () => {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    window.Electron.invoke('get-user').then((data) => {
+      setUser(data);
+    });
+  }, []);
+
   return (
     <div className="bg-blue-100 p-4">
       <h1>Queue Status</h1>
+      <div>
+      {user ? (
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
     </div>
   );
 };
 
 export default QueueStatus;
+
