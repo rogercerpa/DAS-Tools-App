0ca7683cb4dc81ba8d64d0a4be64e3ae9f88bd1a
rogercerpa <roger.e.cerpa@gmail.com>
2023-06-13 18:43:05 -0400

    activated home page for build test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react"
-// import Home from "./Pages/Home"
+import Home from "./Pages/Home"
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* <Home /> */}
-        <h1>the DAS team app - test message</h1>
+        <Home />
       </div>
     </BrowserRouter>
   );
