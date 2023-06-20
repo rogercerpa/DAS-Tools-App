e783940a7f3cc71fb34174514d553cd6d1866b35
rogercerpa <roger.e.cerpa@gmail.com>
2023-06-19 23:14:21 -0400

    added comments to the tools page for instruction and descriptions for its intend

--- src/Pages/Tools.js
+++ src/Pages/Tools.js
@@ -3,13 +3,18 @@ import Sidebar from '../Components/Sidebar';
 import Coordination from "../Features/Coordination"
 import ProjectCreation from '../Features/ProjectCreation';
 import Schedule from '../Features/Schedule';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 const Tools = () => {
+  const location = useLocation();
+  
   return (
     <div className="flex min-h-screen"> 
         <Sidebar />
+        
         <div className="flex-grow">
+        <h1>DAS Productivity Tools</h1>
+        {location.pathname === '/Tools' && <p>Select a tool from the Tools list on the left.</p>}
           <Routes>
             <Route path="Coordination" element={<Coordination />} />
             <Route path="ProjectCreation" element={<ProjectCreation />} />
