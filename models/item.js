85eca64834aba1036b23bd8f0b05c71a18f71cf9
rogercerpa <roger.e.cerpa@gmail.com>
2023-06-27 21:59:04 -0400

    updated server, added schema file for the csv file upload

---
+++ models/item.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    'WQ #': Number,
+    'Task Type': String,
+    'Doc #': String,
+    'Rep': Number,
+    'Status': String,
+    'Assigned To': String,
+    'Due Date': Date,
+    'Requested Date': Date,
+    'Job Name': String,
+    'Est. Hours': Number,
+    'Complexity': String,
+    'User Comment': String,
+    'Subtask': String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const port = 3000;
 const multer = require('multer');
 const csv = require('csv-parser');
 const fs = require('fs');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const Item = require('./models/item');
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 
 // Configure Multer to store files in the 'uploads' directory
@@ -16,29 +18,22 @@ const upload = multer({ dest: 'uploads/' });
 
 // Define an endpoint for uploading CSV files
 app.post('/upload', upload.single('file'), (req, res) => {
-    const results = [];
-
     fs.createReadStream(req.file.path)
         .pipe(csv())
-        .on('data', (data) => results.push(data))
+        .on('data', async (data) => {
+            const item = new Item(data);
+            await item.save();
+        })
         .on('end', () => {
-            // You would typically store the data in your database here
-
-            // For simplicity, we will just send the data back in the response
-            res.json(results);
+            res.status(200).send('CSV file uploaded and parsed successfully');
         });
 });
 
-app.get('/data', (req, res) => {
-    // You would typically fetch this data from your database and send it in the response
-
-    // For simplicity, we will send a hardcoded response
-    res.json([
-        { name: 'Item 1', price: 99.99 },
-        { name: 'Item 2', price: 89.99 },
-        { name: 'Item 3', price: 79.99 },
-    ]);
+app.get('/data', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
 });
+
 
 
 app.listen(port, () => {
