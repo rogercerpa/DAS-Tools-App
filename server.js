3d17697bac0e3282bd679b2e296ddeedeec777b2
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-05 19:02:55 -0400

    updated server, it now deletes all files in database and local directory exept current file uploaded

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const Item = require('./models/item');
 const cors = require('cors');
+const path = require('path');
 
 
 app.use(cors());
@@ -21,19 +22,29 @@ const upload = multer({ dest: 'uploads/' });
 
 // Define an endpoint for uploading CSV files
 app.post('/upload', upload.single('file'), async (req, res) => {
-    // Delete all existing items from the collection
-    await Item.deleteMany({});
-  
-    fs.createReadStream(req.file.path)
+  // Delete all existing items from the collection
+  await Item.deleteMany({});
+
+  // Delete all files in the 'uploads' directory except the current one
+  const files = await fs.promises.readdir('uploads');
+  await Promise.all(files.map(file => {
+      const filePath = path.join('uploads', file);
+      if (filePath !== req.file.path) {
+          return fs.promises.unlink(filePath);
+      }
+  }));
+
+  fs.createReadStream(req.file.path)
       .pipe(csv())
       .on('data', async (data) => {
-        const item = new Item(data);
-        await item.save();
+          const item = new Item(data);
+          await item.save();
       })
       .on('end', () => {
-        res.status(200).send('CSV file uploaded and parsed successfully');
+          res.status(200).send('CSV file uploaded and parsed successfully');
       });
-  });
+});
+
   
 
 app.get('/data', async (req, res) => {
