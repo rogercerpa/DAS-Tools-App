27c3b2ea3837ca28753ca8231660ac35d9d8b9dc
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-05 18:32:02 -0400

    deleteing all file/data before uploading new data

--- server.js
+++ server.js
@@ -20,17 +20,21 @@ mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnified
 const upload = multer({ dest: 'uploads/' });
 
 // Define an endpoint for uploading CSV files
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), async (req, res) => {
+    // Delete all existing items from the collection
+    await Item.deleteMany({});
+  
     fs.createReadStream(req.file.path)
-        .pipe(csv())
-        .on('data', async (data) => {
-            const item = new Item(data);
-            await item.save();
-        })
-        .on('end', () => {
-            res.status(200).send('CSV file uploaded and parsed successfully');
-        });
-});
+      .pipe(csv())
+      .on('data', async (data) => {
+        const item = new Item(data);
+        await item.save();
+      })
+      .on('end', () => {
+        res.status(200).send('CSV file uploaded and parsed successfully');
+      });
+  });
+  
 
 app.get('/data', async (req, res) => {
     const items = await Item.find();
