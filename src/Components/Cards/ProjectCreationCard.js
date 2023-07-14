1597b26ff9906f2af0d39d802e50e2b5c53298f0
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-13 22:25:20 -0400

    populated the data to the table

--- src/Components/Cards/ProjectCreationCard.js
+++ src/Components/Cards/ProjectCreationCard.js
@@ -20,6 +20,7 @@ import {
 import Menu from "../Menu"
 
 export default function ProjectCreationCard({TABS,TABLE_HEAD, TABLE_ROWS }) {
+  console.log(TABLE_ROWS)
   return (
     <Card className="h-full w-full p-5">
       <CardHeader floated={false} shadow={false} className="rounded-none">
@@ -75,69 +76,30 @@ export default function ProjectCreationCard({TABS,TABLE_HEAD, TABLE_ROWS }) {
             </tr>
           </thead>
           <tbody>
-            {TABLE_ROWS.map(({ img, name, email, job, org, online, date }, index) => {
-              const isLast = index === TABLE_ROWS.length - 1;
-              const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
- 
-              return (
-                <tr key={name}>
-                  <td className={classes}>
-                    <div className="flex items-center gap-3">
-                      <Avatar src={img} alt={name} className="h-5 w-5" />
-                      <div className="flex flex-col">
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                          {name}
-                        </Typography>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal opacity-70"
-                        >
-                          {email}
-                        </Typography>
-                      </div>
-                    </div>
-                  </td>
-                  <td className={classes}>
-                    <div className="flex flex-col">
-                      <Typography variant="small" color="blue-gray" className="font-normal">
-                        {job}
-                      </Typography>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal opacity-70"
-                      >
-                        {org}
-                      </Typography>
-                    </div>
-                  </td>
-                  <td className={classes}>
-                    <div className="w-max">
-                      <Chip
-                        variant="ghost"
-                        size="sm"
-                        value={online ? "online" : "offline"}
-                        color={online ? "green" : "blue-gray"}
-                      />
-                    </div>
-                  </td>
-                  <td className={classes}>
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                      {date}
-                    </Typography>
-                  </td>
-                  <td className={classes}>
-                    <Tooltip content="Edit User">
-                      <IconButton variant="text" color="blue-gray">
-                        <PencilIcon className="h-4 w-4" />
-                      </IconButton>
-                    </Tooltip>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
+  {TABLE_ROWS.map((row, rowIndex) => {
+    const isLast = rowIndex === TABLE_ROWS.length - 1;
+    const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+    return (
+      <tr key={rowIndex}>
+        {TABLE_HEAD.map((key, index) => (
+          <td key={index} className={classes}>
+            <Typography variant="small" color="blue-gray" className="font-normal">
+              {row[key]}
+            </Typography>
+          </td>
+        ))}
+        <td className={classes}>
+          <Tooltip content="Edit User">
+            <IconButton variant="text" color="blue-gray">
+              <PencilIcon className="h-4 w-4" />
+            </IconButton>
+          </Tooltip>
+        </td>
+      </tr>
+    );
+  })}
+</tbody>
         </table>
       </CardBody>
       <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
--- src/Features/ToolsFeatures/ProjectCreation.js
+++ src/Features/ToolsFeatures/ProjectCreation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useFetchData } from '../../api/fetchData';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import ProjectCreationCard from "../../Components/Cards/ProjectCreationCard"
 
 const TABS = [
   {
@@ -18,85 +19,26 @@ const TABS = [
     value: "unmonitored",
   },
 ];
- 
-const TABLE_HEAD = ["Project Name", "Due Date", "Requested Data", "Status", "Task Type", "Action"];
- 
-const TABLE_ROWS = [
-  {
-    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
-    name: "John Michael",
-    email: "john@creative-tim.com",
-    job: "Manager",
-    org: "Organization",
-    online: true,
-    date: "23/04/18",
-  },
-  {
-    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
-    name: "Alexa Liras",
-    email: "alexa@creative-tim.com",
-    job: "Programator",
-    org: "Developer",
-    online: false,
-    date: "23/04/18",
-  },
-  {
-    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
-    name: "Laurent Perrier",
-    email: "laurent@creative-tim.com",
-    job: "Executive",
-    org: "Projects",
-    online: false,
-    date: "19/09/17",
-  },
-  {
-    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
-    name: "Michael Levi",
-    email: "michael@creative-tim.com",
-    job: "Programator",
-    org: "Developer",
-    online: true,
-    date: "24/12/08",
-  },
-  {
-    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
-    name: "Richard Gran",
-    email: "richard@creative-tim.com",
-    job: "Manager",
-    org: "Executive",
-    online: false,
-    date: "04/10/21",
-  },
-];
- 
-export default function ProjectCreation() {
 
+export default function ProjectCreation() {
   const { data, isLoading } = useFetchData();
 
   if (isLoading) {
-    return  ( 
-    <Box sx={{ display: 'flex' }}>
-    <CircularProgress />
-  </Box>)
+    return ( 
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+    )
   }
 
+  const wantedKeys = ['Job Name','Assigned To', 'Due Date','Task Type', 'Status'];
+  const TABLE_HEAD = wantedKeys.filter(key => key in data[0]);
+
   return (
-    <div>
-      {data.map((item, index) => (
-        <div key={index}>
-          {console.log(item)}
-        </div>
-      ))}
-    </div>
+    <ProjectCreationCard
+      TABS={TABS}
+      TABLE_HEAD={TABLE_HEAD}
+      TABLE_ROWS={data}
+    />
   );
-
-
-
-  // return (
-  //   <ProjectCreationCard
-  //   TABS={TABS}
-  //   TABLE_HEAD={TABLE_HEAD}
-  //   TABLE_ROWS={TABLE_ROWS}
-  //   />
-  // );
 }
