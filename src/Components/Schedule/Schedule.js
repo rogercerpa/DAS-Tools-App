084a9c3e72b3697fe693b2f9389e6f3383ff4a7a
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-17 22:03:37 -0400

    added calendar view component

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -1,9 +1,19 @@
 
 import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' 
 
 function Schedule() {
   return (
-<main>calendar</main>
+    <FullCalendar
+    plugins={[ dayGridPlugin ]}
+    initialView="dayGridMonth"
+    weekends={false}
+    events={[
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' }
+    ]}
+  />
   )
 }
 
