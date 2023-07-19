068215e4e41ec67d833a15bb0069cf8e1a691861
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-18 21:38:51 -0400

    updated header logo image, add hero to the landing page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -128,8 +128,8 @@ const Header = () => {
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
+                src={Logo}
+                alt="Logo"
               />
             </Link>
             <button
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import DataImport from "../Components/DataImport"
-
+import Hero from "../Components/Hero"
 const Team = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-semibold text-black ">Welcome to the DAS Team HomePage</h1>
-      <DataImport/>
+      <Hero />
+
+      {/* <DataImport/> */}
     </div>
   );
 };
