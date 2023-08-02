62c0880f211d6d4c7435fff9d325bda634195a22
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-02 14:52:52 -0400

    once data imports, input field clears out

--- src/Components/DataImport.js
+++ src/Components/DataImport.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios'; // import axios
 import PopUp from "./PopUp"
 
@@ -8,6 +8,8 @@ const DataImport = () => {
   const [showPopup, setShowPopup] = useState(false);
 
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const inputRef = useRef();
 
   const fileSelectedHandler = event => {
     setSelectedFile(event.target.files[0]);
@@ -30,31 +32,18 @@ const DataImport = () => {
 
       console.log(response.data);
       setShowPopup(true);
+      inputRef.current.value = '';
+      setSelectedFile(null);
     } catch (error) {
       console.error('Error uploading file: ', error);
     }
   };
 
-  const handleClick = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/data');
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  }
-  
-
   return (
 <div className="flex flex-col items-center justify-center space-y-4">
-      {/* <button
-        onClick={handleClick}
-        className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
-      >
-        Load CSV Data
-      </button> */}
+
       <div className="flex items-center space-x-4">
-        <input type="file" onChange={fileSelectedHandler} className="bg-white border p-2 rounded" />
+        <input ref={inputRef} type="file" onChange={fileSelectedHandler} className="bg-white border p-2 rounded" />
         <button
           onClick={fileUploadHandler}
           className="bg-sky-400 hover:bg-sky-500 text-white font-bold py-2 px-4 rounded"
