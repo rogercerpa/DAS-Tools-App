4c535d7fa53184b6a5d0dcaf47a7bcbd30ce050e
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-04 19:03:31 -0400

    updated the sidebar component styling

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -50,22 +50,24 @@ const Sidebar = ({ currentPage }) => {
 const solutions = solutionsForPage[currentPage];
 
   return (
-<div className="w-screen max-w-xs flex-auto overflow-hidden   text-xs leading-6 shadow-sm ring-1 ring-gray-900/5 flex flex-col h-screen">
+<div className="w-fit md:max-w-xs flex-auto overflow-hidden text-xs leading-6 shadow-sm ring-1 ring-gray-900/5 flex flex-col h-screen">
   <div className="p-4 overflow-auto flex-grow">
-    <h1 className='font-semibold text-gray-900 text-xl'>{currentPage}</h1>
+  <h1 className='font-semibold text-gray-900 text-sm md:text-md lg:text-2xl'>
+  {currentPage}
+</h1>
     {solutions.map((item) => (
+      <Link to={item.href} className="font-semibold text-gray-900">
       <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
         <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
           <item.icon className="h-6 w-6 text-gray-600 group-hover:text-sky-600" aria-hidden="true" />
         </div>
-        <div>
-          <Link to={item.href} className="font-semibold text-gray-900">
+        <div className='hidden lg:block'>
             {item.name}
-            <span className="absolute inset-0" />
-          </Link>
+            <span className="absolute inset-0" />    
           <p className="mt-1 text-gray-600">{item.description}</p>
         </div>
       </div>
+      </Link>
     ))}
   </div>
 </div>
