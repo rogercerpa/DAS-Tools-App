ec4caf62250faabc00e24e1834b6613bc3838406
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-07 18:11:03 -0400

    updated the createfolderbutton component to show a popup message once folder is created, need to update the popup content next

--- src/Components/CreateFolderButton.js
+++ src/Components/CreateFolderButton.js
@@ -1,33 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import PopUp from "./PopUp"
 
 const CreateFolder = () => {
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
 
   const handleClick = async () => {
     const folderPath = 'C://Users//rxc04//OneDrive - Acuity Brands, Inc//Desktop//DAS.TOOL.TestFolder';
     const templateFolderPath = 'K://Projects//!!!Templates For Project Creator//RFA#_TYPE_MMDDYYYY';
     window.electron.ipcRenderer.send('create-folder-and-files', { folderPath, templateFolderPath });
   }
-
+ 
   // Listener for the IPC response
   React.useEffect(() => {
-    window.electron.ipcRenderer.on('folder-files-response', (event, arg) => {
+    const handleFolderFilesResponse = (event, arg) => {
       if (arg.success) {
-        console.log('Folder and files created successfully');
+        setShowPopup(true);
       } else {
-        console.error('An error occurred:', arg.error);
+        // Handle failure case here if needed
       }
-    });
+    };
+
+    window.electron.ipcRenderer.on('folder-files-response', handleFolderFilesResponse);
+
 
     // Clean up the listener when the component unmounts
     return () => {
-      window.electron.ipcRenderer.removeAllListeners('folder-files-response');
-    }
+      window.electron.ipcRenderer.removeListener('folder-files-response', handleFolderFilesResponse);
+    };
   }, []);
 
   return (
     <div>
       <button className='bg-sky-400 hover:bg-sky-500 text-white font-bold py-2 px-4 rounded' onClick={handleClick}>Create Folder</button>
+      <PopUp showPopup={showPopup} onClose={handlePopupClose} />
     </div>
   )
 }
