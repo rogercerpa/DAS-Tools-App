f74da2fd490a445c796ec5c62df7e5d44844624b
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-15 16:30:52 -0400

    updated queuestatus with triaged not strted task

--- src/Features/DashboardFeatures/QueueStatus.js
+++ src/Features/DashboardFeatures/QueueStatus.js
@@ -1,39 +1,44 @@
 import React from 'react';
-import SimpleBarChart from "../../Components/Charts/SimpleBarChart"
 import { useFetchData } from '../../api/fetchData';
 
 const QueueStatus = () => {
-  const { data, isLoading } = useFetchData();
-   // Calculate today's date in the format yyyy-mm-dd
-   const todayDate = new Date().toISOString().split('T')[0];
+  const { data } = useFetchData();
+  const todayDate = new Date().toISOString().split('T')[0];
 
   const totalQuantity = data ? data.length : 0;
   const waitingOnRepTasks = data ? data.filter(item => item.Status === "Waiting on Rep") : [];
   const totalWaitingOnRep = waitingOnRepTasks.length;
 
-  const RequestedDateTasks = data
-  ? data.filter(item => item['Requested Date'] <= todayDate)
-  : [];
-const totalRequestedDate = RequestedDateTasks.length;
+  const requestedDateTasks = data
+    ? data.filter(item => item['Requested Date'] <= todayDate)
+    : [];
+  const totalRequestedDate = requestedDateTasks.length;
+
+  const controlsLeadTriageNotStartedTasks = data
+    ? data.filter(item => item['Task Type'] === "Controls Lead Triage" && item.Status === "Not Started")
+    : [];
+  const totalControlsLeadTriageNotStarted = controlsLeadTriageNotStartedTasks.length;
 
   return (
-    <div>
-      <div className="p-4">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">QueueStatus</h1>
-      </div>
-      <div className='flex items-center'>
-        <div className="ml-4">
-          <p className="text-lg font-semibold text-gray-900">Total Tasks in Queue</p>
-          <p className="text-3xl font-bold text-gray-900">{totalQuantity}</p>
-          <p className="text-lg font-semibold text-gray-900">Total Tasks WOR</p>
-          <p className="text-3xl font-bold text-gray-900">{totalWaitingOnRep}</p>
-          <p className="text-lg font-semibold text-gray-900">RD Today or Before</p>
-          <p className="text-3xl font-bold text-gray-900">{totalRequestedDate}</p>
-        </div>
+    <div className="p-4">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">QueueStatus</h1>
+      <div className='flex items-center ml-4'>
+        <TaskStat title="Total Tasks in Queue" quantity={totalQuantity} />
+        <TaskStat title="Total Tasks WOR" quantity={totalWaitingOnRep} />
+        <TaskStat title="RD Today or Before" quantity={totalRequestedDate} />
+        <TaskStat title="Triages Not Started" quantity={totalControlsLeadTriageNotStarted} />
       </div>
     </div>
   );
 };
 
+const TaskStat = ({ title, quantity }) => (
+  <div className="ml-4 bg-slate-500 rounded p-1">
+    <p className="text-lg font-semibold text-gray-900">{title}</p>
+    <p className="text-center text-5xl font-bold text-gray-900">{quantity}</p>
+  </div>
+);
+
 export default QueueStatus;
+
 
