57955f6b2671d6b6a3374b0627ddca493ebeb466
rogercerpa <roger.e.cerpa@gmail.com>
2023-08-31 12:33:55 -0400

    qc card component updated, data is showing

--- src/Components/Cards/QCCard.js
+++ src/Components/Cards/QCCard.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-function QCCard({totalQCTasks}) {
+function QCCard({ totalQCTasks}) {
+  const getStatusTasks = (status) => {
+    return totalQCTasks.filter(task => task.Status === status);
+  };
+
+  const renderTable = (statusTasks) => (
+    
+    <table className="min-w-full bg-white">
+      <thead>
+        <tr>
+          <th className="w-1/3 px-4 py-2">Task Name</th>
+          <th className="w-1/3 px-4 py-2">Assigned to</th>
+          {/* You can add more columns if needed */}
+        </tr>
+      </thead>
+      <tbody>
+        {statusTasks.map(task => (
+          <tr key={task.id}>
+            <td className="border px-4 py-2">{task['Job Name']}</td>
+            <td className="border px-4 py-2">{task['Assigned To']}</td>
+            {/* You can add more columns if needed */}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    
+  );
+
   return (
-    <div>QCCard</div>
-  )
+    <div className="space-y-6">
+      <h2 className="text-xl font-bold mb-2">Waiting on Rep</h2>
+      {renderTable(getStatusTasks('Waiting on Rep'))}
+
+      <h2 className="text-xl font-bold mb-2">Not Started</h2>
+      {renderTable(getStatusTasks('Not Started'))}
+
+      <h2 className="text-xl font-bold mb-2">In Progress</h2>
+      {renderTable(getStatusTasks('In Progress'))}
+    </div>
+  );
 }
 
-export default QCCard
+export default QCCard;
