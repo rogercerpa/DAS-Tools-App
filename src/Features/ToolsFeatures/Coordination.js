f753a8085f0ef3fbf60a9e6d934035df03c629ca
rogercerpa <roger.e.cerpa@gmail.com>
2023-10-30 23:50:34 -0400

    updated the coordination and controls news pages

--- src/Features/ToolsFeatures/Coordination.js
+++ src/Features/ToolsFeatures/Coordination.js
@@ -3,9 +3,27 @@ import React from 'react';
 
 const Coordination = () => {
 
+  const stats = [
+    { id: 1, name: 'Triage Hours', value: '23 Hours' },
+    { id: 2, name: 'SAE Online', value: '6' },
+    { id: 3, name: 'QC Hours', value: '10 Hours' },
+  ]
 
   return (
-<div>Coordination</div>
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          {stats.map((stat) => (
+            <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
+              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                {stat.value}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
   );
 };
 
