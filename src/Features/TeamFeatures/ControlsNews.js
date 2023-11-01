30b9ad5a6aad58c5801a21ff0b379cb41b1d70b4
rogercerpa <roger.e.cerpa@gmail.com>
2023-10-31 23:06:38 -0400

    updated styling for controls news page

--- src/Features/TeamFeatures/ControlsNews.js
+++ src/Features/TeamFeatures/ControlsNews.js
@@ -25,8 +25,8 @@ function ControlsNews() {
 
   return (
     <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
+      <div className="mx-auto max-w-4xl px-6 ">
+        <div className="mx-auto  lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Acuity Controls News</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             Here you will find the latests news about Acuity Lighting Controls.
